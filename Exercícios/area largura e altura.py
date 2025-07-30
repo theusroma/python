e0b2525c70b86186c18cc774389c0850b1b5780b
Matheus Santos <142417170+theusroma@users.noreply.github.com>
2025-07-30 11:26:12 -0300

    Create area largura e altura.py

---
+++ Exercícios/area largura e altura.py
@@ -0,0 +1,6 @@
+largura = float(input("Digite a largura da area a ser pintada: "));
+altura = float(input("Digite a altura da area a ser pintada: "));
+
+area = largura * altura;
+
+print("A quantidade de metros eh: ", area);
