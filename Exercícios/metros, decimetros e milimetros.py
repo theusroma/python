0b3cec4cae8796308a0c09972c87763ceb6f01bb
Matheus Santos <142417170+theusroma@users.noreply.github.com>
2025-07-30 11:27:10 -0300

    Create metros, decimetros e milimetros.py

---
+++ Exercícios/metros, decimetros e milimetros.py
@@ -0,0 +1,9 @@
+metros = float(input("Digite a quantidade em metros: "));
+decimetros = metros * 10;
+centimetros = metros * 100;
+milimetros = metros * 1000;
+
+print("Decimetros: ", decimetros);
+print("centimetros: ", centimetros);
+print("milimetros: ", milimetros);
+
