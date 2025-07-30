786c0991ad00c7bba5a5470fbffa6fb3c5df870f
Matheus Santos <142417170+theusroma@users.noreply.github.com>
2025-07-30 11:27:23 -0300

    Create tabuada.py

---
+++ Exercícios/tabuada.py
@@ -0,0 +1,35 @@
+print("Tabuada do 1")
+for i in range(1, 10):
+    print(i)
+
+print("Tabuada do 2")
+for i in range(1, 10):
+    print(i * 2)
+
+print("Tabuada do 3")
+for i in range(1, 10):
+    print(i * 3)
+
+print("Tabuada do 4")
+for i in range(1, 10):
+    print(i * 4)
+
+print("Tabuada do 5")    
+for i in range(1, 10):
+    print(i * 5)
+
+print("Tabuada do 6")    
+for i in range(1, 10):
+    print(i * 6)
+
+print("Tabuada do 7")    
+for i in range(1, 10):
+    print(i * 7)
+
+print("Tabuada do 8")    
+for i in range(1, 10):
+    print(i * 8)
+
+print("Tabuada do 9")    
+for i in range(1, 10):
+    print(i * 9)
