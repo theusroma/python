f967d247f7a332881477375664ec0525e721ded2
Matheus Santos <142417170+theusroma@users.noreply.github.com>
2025-08-13 11:11:24 -0300

    Add files via upload

---
+++ Exercícios/lista exemplo.py
@@ -0,0 +1,138 @@
+# x = [10, 5, 11, 0, 3]
+#print(x)
+
+
+
+#l = [1, 45, 'abc', 3 + 4j, [1, 2, 3, 4]]
+#print(l)
+
+
+
+#concatenando listas
+#a = [1,2,3]; b = [4,5,6]
+#c = a + b
+#print(c)
+
+
+
+#apend serve para acrescentar conteudo em listas ja criadas
+#c = [1,2,3,4,5,6,7]
+#print(c)
+#c.append('abc')
+#print(c)
+
+
+
+#clear serve para limpar a lista 
+#x = [1,2,3]
+#print(x)
+#x.clear()
+
+
+
+#count serve para contar quantas vezes o elemento apareceu no indice
+#x = [4, 5, 6, 8, 4, 5, 4, 6, 4]
+#print(x.count(4))
+
+
+
+#retorna o numero do indice que o elemento esta
+#x = [1, 2, 3, 4, 5]
+#print(x.index(5))
+
+
+
+#inserir elemento em posicao especifica da lista
+#x = ['a', 'c', 'd']
+#print(x)
+#x.insert(1, 'b')
+#print(x)
+
+
+
+#deleta elemento do arranjo indicado na lista
+#x = ['a', 'b', 'c', 'd']
+#print(x)
+#x.pop(2)
+#print(x)
+
+
+
+#exclui o elemento selecionado
+#x = ['a', 'b', 'c', 'C']
+#print(x)
+#x.remove('c')
+#print(x)
+
+
+
+#inverte a ordem do indice
+#x = [5, 4, 3, 4, 1]
+#print(x)
+#x.reverse()
+#print(x)
+
+
+
+#ordena a lista em ordem crescente numérica ou ordfem alfabética
+#x = [10, 3, 3, 10, 5, 6, 6, 4, 8, 7]
+#print(x)
+#x.sort()
+#print(x)
+
+
+
+#somar numeros nos elementos dos indices
+#a=[3, 4, 5, 10]
+#soma1 = list()
+#for i in a:
+
+#   soma1 += [i+1]
+
+#print(soma1)
+
+
+
+#a = [0, 0, 5, 3, 2, 7, 4, 10, 4]
+#n = list()
+#print(n)
+#for i in a:
+#    if i % 2 == 0:
+#        n += [i*i]
+#print(n)
+
+
+#mostra o numeor entre os outros
+#x=[1,2,3,4,5,6,7,8]
+#print(x)
+#print(x[0:2:1])
+
+#print(x[0:4:1])
+
+#print(x[0:4:2])
+
+
+
+#tuplas exemplo
+#x=(1,2,3)
+#y=(1,2,4)
+#z=(4,0,0)
+
+#x<y
+#True
+
+#x<z
+#True
+
+#print(x + y)
+
+
+
+
+#dicionario --usa chaves--
+#x={'idade': 21, 'matricula': 1000, 'nome': 'Joao'}
+#print(x['idade'])
+#print(x['matricula'])
+#print(x['nome'])
+
+
